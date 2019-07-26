89a9d37b888a25e12812af5edace0a77d3773fbd
jbwang <wangjiangbo@sudiyi.cn>
2019-07-26 14:25:23 +0800

    test

--- src/main/java/com/sudiyi/apple/controller/HelloController.java
+++ src/main/java/com/sudiyi/apple/controller/HelloController.java
@@ -1,4 +1,11 @@
 package com.sudiyi.apple.controller;
 
+/**
+ * @author sdy
+ */
 public class HelloController {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
 }
