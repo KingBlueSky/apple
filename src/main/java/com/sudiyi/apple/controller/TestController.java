699eb6039b492407f02ac8a1bf692e37a128812f
jbwang <wangjiangbo@sudiyi.cn>
2019-07-26 14:27:02 +0800

    test add

---
+++ src/main/java/com/sudiyi/apple/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.sudiyi.apple.controller;
+
+/**
+ * @author sdy
+ */
+public class TestController {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
